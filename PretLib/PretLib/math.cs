7d6b6bd740d10e53958251bbefb08925da9dc94a
Pret D.B <pret-db@qq.com>
2018-05-17 22:44:41 +0800

    Initiated the normal distributed random number.

---
+++ PretLib/PretLib/math.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace PretLib
+{
+    public class Matrix
+    {
+        public uint nRow;
+        public uint nColumn;
+    }
+
+    public static class Math 
+    {
+        public static Random ranGenerator;
+
+        public static float NormalRandom(float u, float delta, int seed = 0)
+        {
+            return PretLib.Math.NorMalRandomPoint(u, delta, 0f, 0f, seed).X;
+        }
+
+        public static PointF NorMalRandomPoint(float u, float delta, float v, float gamma, int seed = 0)
+        {
+            if (ranGenerator == null)
+            {
+                PretLib.Math.ranGenerator = new Random(seed);
+            }
+
+            delta = System.Math.Abs(delta);
+            gamma = System.Math.Abs(gamma);
+
+            double x = PretLib.Math.ranGenerator.NextDouble();
+            double y = PretLib.Math.ranGenerator.NextDouble();
+
+
+
+            float z0 = (float)(System.Math.Sqrt(-2 * System.Math.Log(x)) * System.Math.Cos(2 * System.Math.PI * y));
+            float z1 = (float)(System.Math.Sqrt(-2 * System.Math.Log(x)) * System.Math.Sin(2 * System.Math.PI * y));
+
+            z0 = z0 * delta + u;
+            z1 = z1 * gamma + v;
+
+            return new PointF(z0, z1);
+        }
+    }
+}
---
+++ PretLib/测试/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PretLib
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PointF[] ps = new PointF[1000];
+            Bitmap map = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+
+            this.listBox1.Items.Clear();
+
+            for(int a = 0; a < ps.Length; a++)
+            {
+                ps[a] = Math.NorMalRandomPoint(this.pictureBox1.Width / 2, 50, this.pictureBox1.Height / 2, 10f, DateTime.Now.Millisecond);
+                map.SetPixel(Convert.ToInt32(ps[a].X), Convert.ToInt32(ps[a].Y), Color.Red);
+                this.listBox1.Items.Add(ps[a].ToString());
+            }
+            this.pictureBox1.Image = map;
+
+
+        }
+    }
+}
